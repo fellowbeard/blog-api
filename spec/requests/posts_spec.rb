516daccac2f62ee5b827ff3f8195ae8b82a8ad0a
fellowbeard <virgovibe@gmail.com>
2022-05-23 13:42:43 -1000

    updates to spec file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Posts", type: :request do
 
       user = User.create!(name: "Test", email: "test@test.com", password: "password")
       
-      Post.create!(user_id: 1, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
 
       get "/posts"
       posts = JSON.parse(response.body)
@@ -21,17 +21,15 @@ RSpec.describe "Posts", type: :request do
 
       user = User.create!(name: "Test", email: "test@test.com", password: "password")
       
-      Post.create!(user_id: 1, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
 
       get "/posts/#{post.id}"
       post = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
-      # expect(post).
-
-
-
+      expect(post["image"]).to eq("https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+    end
   end
-  end
+  
 
 end
